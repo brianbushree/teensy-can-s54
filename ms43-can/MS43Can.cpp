ecd2ed5c1b57a5f5bd660030d6805a93b35c6848
Brian Bushree <bbushree@confluent.io>
2023-09-16 15:40:39 -0700

    Complete DME2 class

--- ms43-can/MS43Can.cpp
+++ ms43-can/MS43Can.cpp
@@ -53,6 +53,86 @@ double MS43_DME1_Frame::theorethicalTorquePercent() {
 
 MS43_DME2_Frame::MS43_DME2_Frame(uint8_t (&f)[8]) : frame(f) {};
 
+double MS43_DME2_Frame::engineTempC() {
+    return (frame[1] * .75) - 48.373;
+}
+
+double MS43_DME2_Frame::engineTempF() {
+    return (engineTempC() * 1.8) + 32;
+}
+
+int MS43_DME2_Frame::ambientPressure() {
+    if (frame[2] == 0x00) {
+        return 0;
+    } else if (frame[2] == 0xFF) {
+        return -1;
+    } else {
+        return frame[2] != 0xFF ? (frame[2] * 2) + 598 : -1;
+    }
+}
+
+bool MS43_DME2_Frame::clutchIsPressed() {
+    return frame[3] & 0b00000001;
+}
+
+bool MS43_DME2_Frame::idleRegulatorOn() {
+    return frame[3] & 0b00000010;
+}
+
+bool MS43_DME2_Frame::ackFromACC1() {
+    return frame[3] & 0b00000100;
+}
+
+bool MS43_DME2_Frame::engineIsRunning() {
+    return frame[3] & 0b00001000;
+}
+
+MS43_DME2_STEERING_WHEEL_CRUISE_STATE MS43_DME2_Frame::steeringWheelCruiseState() {
+    return static_cast<MS43_DME2_STEERING_WHEEL_CRUISE_STATE>((frame[3] & 0b11100000) >> 5);
+}
+
+// Helper function for pedal values
+double pedalValueToPercent(uint8_t val) {
+    // init and zero values
+    if (val == 0x00 || val == 0x01) {
+        return 0;
+    } else if (val == 0xFF) {
+        return -1;
+    } else {
+        return val * 0.390625;
+    }
+}
+
+double MS43_DME2_Frame::virtualCruisePedalPercent() {
+    return pedalValueToPercent(frame[4]);
+}
+
+double MS43_DME2_Frame::acceleratorPedalPercent() {
+    return pedalValueToPercent(frame[5]);
+}
+
+bool MS43_DME2_Frame::brakeSwitchActive() {
+    return frame[6] & 0b00000001;
+}
+
+bool MS43_DME2_Frame::brakeSwitchHasError() {
+    return frame[6] & 0b00000010;
+}
+
+bool MS43_DME2_Frame::kickdownIsActive() {
+    return frame[6] & 0b00000100;
+}
+
+MS43_DME2_CRUISE_STATE MS43_DME2_Frame::cruiseControlState() {
+    return static_cast<MS43_DME2_CRUISE_STATE>((frame[6] & 0b00111000) >> 3);
+
+}
+
+bool MS43_DME2_Frame::shiftLockRequested() {
+    return frame[6] & 0b11000000;
+}
+
 MS43_DME3_Frame::MS43_DME3_Frame(uint8_t (&f)[8]) : frame(f) {};
 
 MS43_DME4_Frame::MS43_DME4_Frame(uint8_t (&f)[8]) : frame(f) {};
+
--- ms43-can/MS43Can.h
+++ ms43-can/MS43Can.h
@@ -5,19 +5,19 @@
 typedef enum MS43_DME1_CHARGE_INTRV_STATE {
   // the required intervention can be performed completely
   // or if there is no ASC requirement
-  INTRV_POSSIBLE,
+  CIS_INTRV_POSSIBLE,
 
   // the required intervention cannot be performed completely
   // because the IGA retard limit is reached
-  INTRV_NOT_POSSIBLE_IGA_LIMIT,
+  CIS_INTRV_NOT_POSSIBLE_IGA_LIMIT,
 
   // the required intervention cannot be performed completely
   // because the charge actuators are fully closed (this case is
   // not possible with MTC without break-away mechanism),
-  INTRV_NOT_POSSIBLE_ACT_CLOSED,
+  CIS_INTRV_NOT_POSSIBLE_ACT_CLOSED,
 
   // "limited vehicle dynamics" is active (MTC/ISA error)
-  LIMITED_VEH_DYNAMICS_ACTIVE
+  CIS_LIMITED_VEH_DYNAMICS_ACTIVE
 } MS43_DME1_CHARGE_INTRV_STATE;
 
 // DME1 : 0x316
@@ -88,6 +88,54 @@ class MS43_DME1_Frame {
     double theorethicalTorquePercent();
 };
 
+typedef enum MS43_DME2_STEERING_WHEEL_CRUISE_STATE {
+  // No Button Pressed (Init Value)
+  SW_CRUISE_STATE_NO_BUTTON_PRESSED,
+
+  // Set / Acceleration (tip-up)
+  SW_CRUISE_STATE_INCREASE_OR_SET,
+
+  // Deceleration (tip-down)
+  SW_CRUISE_STATE_DECREASE,
+
+  // Resume
+  SW_CRUISE_STATE_RESUME,
+
+  // Deactivate (I/O)
+  SW_CRUISE_STATE_DEACTIVATE,
+
+  SW_CRUISE_STATE_NOT_USED_5,
+  SW_CRUISE_STATE_NOT_USED_6,
+
+  // Error Condition
+  SW_CRUISE_STATE_ERROR
+} MS43_DME2_STEERING_WHEEL_CRUISE_STATE;
+
+typedef enum MS43_DME2_CRUISE_STATE {
+
+  // LV_CRU_ACT = 0
+  CRUISE_STATE_NOT_ACTIVE,
+
+  // LV_CRU_ACT = 1, constant drive (or tip-up / tip-down)
+  CRUISE_STATE_CONSTANT_DRIVE,
+
+  CRUISE_STATE_NOT_USED_2,
+
+  // LV_CRU_ACT = 1, resume
+  CRUISE_STATE_RESUME,
+
+  CRUISE_STATE_NOT_USED_4,
+
+  // LV_CRU_ACT = 1, set / acceleration
+  CRUISE_STATE_SET_ACCELERATION,
+
+  CRUISE_STATE_NOT_USED_6,
+
+  // LV_CRU_ACT = 1, deceleration
+  CRUISE_STATE_DECELERATION
+
+} MS43_DME2_CRUISE_STATE;
+
 // DME2 : 0x329
 // Refresh Rate: 10ms
 class MS43_DME2_Frame {
@@ -99,6 +147,93 @@ class MS43_DME2_Frame {
 
     // Create a MS43_DME2_Frame view from frame
     MS43_DME2_Frame(uint8_t (&f)[8]);
+
+    // TEMP_ENG:
+    // Engine (Coolant) Temperature in °C
+    // min:      -47.623°C
+    // init/max: 142.877°C
+    double engineTempC();
+
+    // TEMP_ENG:
+    // Engine (Coolant) Temperature in °F
+    // min:      -53.7214°F
+    // init/max: 289.179°F
+    double engineTempF();
+
+    // AMP_CAN:
+    // Ambient Pressure in hPa
+    // init:   0
+    // min:    600hPa
+    // max:    1106hPa
+    // Error:  -1
+    int ambientPressure();
+
+    // LV_SWI_CLU:
+    // Clutch Switch State
+    // false - released
+    // true  - depressed
+    bool clutchIsPressed();
+
+    // LV_LEVEL_IS:
+    // Idle Regulator State
+    // false - idle above threshold
+    // true  - idle below threshold
+    bool idleRegulatorOn();
+
+    // LV_ACK_CRU_AD_ECU:
+    // Acknowledge of ACC1 CAN Message
+    // false - no ACK
+    // true  - ACK
+    bool ackFromACC1();
+
+    // LV_ERU_CAN:
+    // Engine Running State
+    // false - engine stopped
+    // true  - engine running
+    bool engineIsRunning();
+
+    // STATE_MSW_CAN: Steering wheel cruise control buttons
+    // See enum values for reference
+    MS43_DME2_STEERING_WHEEL_CRUISE_STATE steeringWheelCruiseState();
+
+    // TPS_VIRT_CRU_CAN:
+    // Virtual cruise control pedal percentage
+    double virtualCruisePedalPercent();
+
+    // TPS_CAN:
+    // Accelerator Pedal Position in % of PVS_MAX
+    // init/min: 0%
+    // max:    : 99.2%
+    // error   : -1
+    double acceleratorPedalPercent();
+
+    // LV_BS:
+    // Brake Switch State
+    // false - brake not actuated
+    // true  - brake actuated
+    bool brakeSwitchActive();
+
+    // LV_ERR_BS:
+    // Brake Switch System State
+    // false - brake switch system OK
+    // true  - brake switch system faulty
+    bool brakeSwitchHasError();
+
+    // LV_KD_CAN:
+    // Kick Down State
+    // 0 = kick down not active
+    // 1 = kick down active
+    bool kickdownIsActive();
+
+    // STATE_CRU_CAN: Cruise Control state
+    // See enum values for reference
+    MS43_DME2_CRUISE_STATE cruiseControlState();
+
+    // REQ_SHIFTLOCK:
+    // Request for shiftlock to engage
+    // false - no actuation is active
+    // true  - actuation ISA, MTC or N_SP_IS is active
+    bool shiftLockRequested();
 
 };
 
